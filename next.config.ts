03cbf6e87f2d83eb4b4d74cbfbc4eba2210d7aac
Peili Lee <peili123@gmail.com>
2025-09-08 23:37:07 -0500

    RESTART PROJECT with clean slate

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
-
-
 const nextConfig: NextConfig = {
-  // @ts-expect-error – Next.js doesn't export types for this hook
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
