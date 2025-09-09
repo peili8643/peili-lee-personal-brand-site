1d5c564c79d7fc4192d0b7a1cfc33236543d6413
Peili Lee <peili123@gmail.com>
2025-09-08 23:26:47 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from "next";
 
+
+// Minimal type shape for the config object
+type WebpackDevMiddlewareConfig = {
+  watchOptions?: {
+    poll?: number;
+    aggregateTimeout?: number;
+  };
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  webpackDevMiddleware: (config: WebpackDevMiddlewareConfig) => {
+    config.watchOptions = {
+      poll: 1000, // check for file changes every 1s
+      aggregateTimeout: 300, // delay before rebuilding
+    };
+    return config;
+},
 };
 
 export default nextConfig;
