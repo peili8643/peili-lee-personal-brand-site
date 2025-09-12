a6cf61991980bfd8e2acb8ce788d697ca78b349b
Peili Lee <peili123@gmail.com>
2025-09-11 23:21:40 -0500

    Adding image link to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className = "flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">
+            <Image src="/hilink-logo.svg" alt ="logo" width={74} height ={29} />
+        </Link>
+
+    </nav>
   )
 }
 
